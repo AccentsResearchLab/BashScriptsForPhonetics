97c061081f3150aceb93ffa0e98aa083b6d2831b
cesine <cesine@yahoo.com>
2012-05-11 18:27:09 -0400

    adding sample out file for the recursive list of files script

--- recursiveListOfFilesInSubFolders.sh
+++ recursiveListOfFilesInSubFolders.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 [[ -n "$1" ]] || { echo "To use this script to list all files in all subfolders, type ./recursiveListOfFilesInSubFolders.sh  directoryToList outfile.txt "; exit 0 ; }
 
-for d in `find $1 -type d`; do ls $d >> $2 ; done
+for d in `find $1 -type d`; 
+  do ls $d >> $2 ; 
+done
